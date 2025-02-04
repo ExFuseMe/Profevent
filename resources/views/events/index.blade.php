50ec29139febe9c9ef634f555e9ab04ad96dd564
ExFuseMe <dn007228@gmail.com>
2025-02-04 20:12:44 +0300

    refactor index page

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
     <div class="text-white p-4">
-        <div class="grid grid-cols-5 gap-4 text-center">
+        <div class="grid grid-cols-6 gap-4 text-center">
             <div class="">Название</div>
             <div class="">Дата/время</div>
             <div class="">Место проведения</div>
             <div class="">Кол-во зарегистрированных студентов</div>
             <div class="mb-8">Кол-во организаторов</div>
+            <div class=""></div>
         </div>
         <livewire:events />
     </div>
--- resources/views/livewire/events.blade.php
+++ resources/views/livewire/events.blade.php
@@ -1,5 +1,5 @@
-<div class="grid grid-cols-5 gap-4 text-center">
-    @forelse($this->events() as $event)
+<div class="grid grid-cols-6 gap-4 text-center">
+    @foreach($this->events() as $event)
         <div class="text-green-600">
             <a href="{{route('events.show', $event)}}">{{$event->name}}</a>
         </div>
@@ -7,9 +7,15 @@
         <div class="">{{$event->location}}</div>
         <div class="">{{$event->participants->count()}}</div>
         <div class="">{{$event->managers->count()}}</div>
-    @empty
-        <p>Мероприятий больше не найдено</p>
-    @endforelse
+        <div class="">
+            <form action="{{route('event.report', $event)}}" method="POST">
+                @csrf
+                <button class="text-sm bg-green-600 rounded p-2" type="submit">
+                    Сформировать отчёт
+                </button>
+            </form>
+        </div>
+    @endforeach
     <div x-intersect.full="$wire.loadMore()" class="p-4">
         <div wire:loading wire:target="loadMore"
              class="loading-indicator">
